2b97b189d76c6783fa2628cf7a9f65ef49c6eee0
crazykyler005 <k.s.nelson77@gmail.com>
2025-08-24 14:54:38 -0700

    removed migrations file

--- Entity/Game.cs
+++ Entity/Game.cs
@@ -6,12 +6,11 @@ namespace BowlingApp.Entity
 	{
 		[Key]
 		public int Id { get; set; }
-		[Required]
 		public DateTime DateCreated { get; set; } = DateTime.UtcNow;
 
 		// Foreign key
 		[Required]
-		public int UserId { get; set; } = 0;
+		public int UserId { get; set; }
 		// Navigation properties
 		public ICollection<ScoreBoard> Games { get; set; } = new List<ScoreBoard>();
 		public User User { get; set; } = new User();
